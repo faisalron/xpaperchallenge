486dfef9fa747dba8fc24de05e614f4443f0c32b
Faisal Ibrahim Hadiputra <faisalron@users.noreply.github.com>
2019-04-28 12:56:23 +0900

    update based onkatsuoka-san's suggestion

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,36 +16,22 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-
+html, body {
+  height: 100%;
+}
 *,
 *:before,
 *:after {
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+.stretched-link:hover {
   text-decoration: none;
-  padding: 10px 30px;
+  color: rgb(33, 37, 41);
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
+.stretched-link {
   text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+  color: rgb(33, 37, 41);
 }
 .stretched-link::after {
   position: absolute;
@@ -71,7 +57,7 @@ html {
 }
 .content {
   display: flex;
-  min-height: 63vh;
-  flex-direction: column;
+  min-height: 100vh;
+  background-color: #f9f9f9;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,36 @@
 <template>
   <div>
-    <b-jumbotron bg-variant="dark" text-variant="white">
-      <h1 class="text-center">
-        xpaper.challenge
-      </h1>
-      <div class="text-center">
-        〜CV/NLP分野の今を映し、トレンドを創り出す挑戦〜
-      </div>
-    </b-jumbotron>
-    <b-container class="content">
-      <b-col cols="12" md="10" offset-md="1" lg="6" offset-lg="3">
-        <b-card-group deck>
-          <b-card
-            :img-src="require('~/static/image/cvpaperchallenge-logo.png')"
-            class="with-shadow"
-            height="100"
-            img-top
-            img-alt="cvpaperchallenge"
-          >
-            <h4 class="text-center">cvpaper.challenge</h4>
-            <a href="http://xpaperchallenge.org/cv" class="btn stretched-link"></a>
-          </b-card>
-          <b-card
-            :img-src="require('~/static/image/nlpaperchallenge-logo.png')"
-            class="with-shadow"
-            height="100"
-            img-top
-            img-alt="nlpaperchallenge"
-          >
-            <h4 class="text-center">nlpaper.challenge</h4>
-            <a href="http://xpaperchallenge.org/nlp" class="btn stretched-link"></a>
-          </b-card>
-        </b-card-group>
-      </b-col>
+    <b-container class="content" fluid>
+      <b-row align-v="center">
+        <b-col class="text-center">
+          <h1 class="text-center">xpaper.challenge</h1>
+          <div class="text-center">〜CV/NLP分野の今を映し、トレンドを創り出す挑戦〜</div>
+          <b-row>
+            <b-col cols="6" offset="3" sm="8" offset-sm="2" xl="4" offset-xl="4">
+              <b-card-group class="menu" deck>
+                <b-card
+                  :img-src="require('~/static/image/cvpaperchallenge-logo.png')"
+                  height="100"
+                  img-top
+                  img-alt="cvpaperchallenge"
+                >
+                  <a href="http://xpaperchallenge.org/cv" class="stretched-link text-center menu-text">cvpaper.challenge</a>
+                </b-card>
+                <b-card
+                  :img-src="require('~/static/image/nlpaperchallenge-logo.png')"
+                  height="100"
+                  img-top
+                  img-alt="nlpaperchallenge"
+                >
+                  <a href="http://xpaperchallenge.org/nlp" class="stretched-link text-center menu-text">nlpaper.challenge</a>
+                </b-card>
+              </b-card-group>
+            </b-col>
+          </b-row>
+          <div class="text-center quote">To invent, you need a good imagination and a pile of junk - Thomas Alva Edison</div>
+        </b-col>
+      </b-row>
     </b-container>
-    <footer class="footer mt-5">
-      <div class="bg-dark text-white">
-          <h5 class="text-center pt-2 pb-2 mb-0">&copy;2019 xpaperchallenge.org</h5>
-      </div>
-    </footer>
   </div>
 </template>
 
@@ -49,5 +41,14 @@ export default {
 </script>
 
 <style>
-
+.menu {
+  margin-top: 45px;
+}
+.menu-text {
+  font-size: 0.9em;
+}
+.quote {
+  margin-top: 45px;
+  font-style: italic;
+}
 </style>
